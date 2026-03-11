4a0248fa73d90605d55a7818a22289c6bbf2e1f1
PrajuMaharjan <prajumaharjan903@gmail.com>
2026-03-11 12:37:10 +0545

    Tutorial_1 Q5 done

--- Tutorial_1/Q4.c
+++ Tutorial_1/Q4.c
@@ -8,25 +8,25 @@ void main(){
  float temp;
  
  while(true){
- printf("What do you want to convert : \n1.Celsius to Fahrenheit\n2.Fahrenheit to Celsius");
- scanf("%c",&choice);
- if(choice=="1"){
+ printf("What do you want to convert : \n1.Celsius to Fahrenheit\n2.Fahrenheit to Celsius\n");
+ scanf(" %c",&choice);
+ if(choice=='1'){
   printf("Enter temperature in celsius : ");
   scanf("%f",&temp);
-  printf("%f° Celsius = %f° Fahrenheit",temp,temp*1.8+32);
- }else if(choice=="2"){
+  printf("%f° Celsius = %f° Fahrenheit\n",temp,temp*1.8+32);
+ }else if(choice=='2'){
   printf("Enter temperature in fahrenheit : ");
   scanf("%f",&temp);
-  printf("%f° Fahrenheit = %f° Celsius",temp,(temp-32)/1.8);
+  printf(" \n%f° Fahrenheit = %f° Celsius\n",temp,(temp-32)/1.8);
  }else{
-  printf("Please select valid option");
+  printf("Please select valid option\n");
   continue;
  }
- printf("Press s to stop. Press any other character to continue.");
- scanf("%c",&choice);
- if(choice=="s"){
+ printf("Press s to stop. Press any other character to continue.\n");
+ scanf(" %c",&choice);
+ if(choice=='s'){
   break;
  }
  }
- println("\n");
+ printf("\n");
 }
