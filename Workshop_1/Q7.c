37ff4e0013fc7ac8569c3564522f9d73e9f7f531
PrajuMaharjan <prajumaharjan903@gmail.com>
2026-03-11 09:49:39 +0545

    Workshop-1 Q7 done

--- Workshop_1/Q7.c
+++ Workshop_1/Q7.c
@@ -2,13 +2,30 @@
 // a.Extend this program by continuingly asking the user for a number until they type in a prime number to end the script
 
 #include <stdio.h>
+#include <stdbool.h>
+
 void main(){
  int num;
+ 
  while(1){
- for(int i=2;i<=nl;i++){
-  if(num<=1){
-  printf("%d is not a prime number.",n);
+ bool is_prime=true;
+ 
+ printf("Enter a prime number to end this program:");
+ scanf("%d",&num);
+ if(num<=1){
+   printf("%d is not a prime number.\n",num);
+   continue;
+ }
+ for(int i=2;i*i<=num;i++){
+  if(num%i==0){
+   printf("%d is not a prime number.\n",num);
+   is_prime=false;
+   break;
  }
 }
+ if(is_prime){
+  printf("%d is a prime number.\n",num);
+  break;
+ }
 }
 }
