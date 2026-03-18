5c1655065f54876b77c84b72a811940c2646d918
PrajuMaharjan <prajumaharjan903@gmail.com>
2026-03-18 21:39:52 +0545

    Tutorial-2 Q7 done

--- Tutorial_2/Q7.c
+++ Tutorial_2/Q7.c
@@ -3,38 +3,51 @@
 #include <stdio.h>
 #include <math.h>
 
-float force(float mass,float acceleration){
+float calculation(float mass,float acceleration,float force){
  if(isnan(mass)){
-  printf("Mass is not a number.");
-  return NAN;
+  return force/acceleration;
  }else if(isnan(acceleration)){
-  printf("Accelaration is not a number.");
-  return NAN;
- }else{
+  return force/mass;
+ }else if(isnan(force)){
   return mass*acceleration;
+ }else{
+  return -1;
  }
- 
 }
 
 int main(){
- float m,a,f;
+ float m,a,f,result;
  
  while(1){
- printf("Enter mass (): ");
+ printf("Enter mass (enter NAN for unknown value): ");
  scanf("%f",&m);
 
- printf("Enter accelaration : ");
+ printf("Enter accelaration (enter NAN for unknown value): ");
  scanf("%f",&a);
  
- f=force(m,a);
+ printf("Enter force (enter NAN for unknown value): ");
+ scanf("%f",&f);
  
- if(isnan(f)){
+  if(isnan(m)+isnan(a)+isnan(f)>1){
+  printf("Error: More than one value is NAN.\n");
   continue;
- }else{
-  printf("Force = %f",f);
  }
  
+ result=calculation(m,a,f);
+
+ if(isnan(m)){
+  printf("Mass = %f .",result);
+ }else if(isnan(a)){
+  printf("Acceleration = %f .",result);
+ }else if(isnan(f)){
+  printf("Force = %f .",result);
+ }else{
+  printf("Error: Nothing to calculate.\n");
+  continue;
  }
+  break;
+ }
+ 
  printf("\n");
  return 0;
 }
