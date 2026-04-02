3d02f7a19fce856de9114ff22ab17e710db9a640
PrajuMaharjan <prajumaharjan903@gmail.com>
2026-04-02 07:55:20 +0545

    Tutorial-5 Q2 done

--- Tutorial-5/Q2.c
+++ Tutorial-5/Q2.c
@@ -1,1 +1,20 @@
 //2. Create a structure variable.
+
+#include<stdio.h>
+
+struct Student{
+ char name[50];
+ int level;
+ char group[6];
+};
+
+int main(){
+ struct Student s1={"Praju Maharjan",5,"L5CG9"};
+ 
+ printf("Name : %s\n",s1.name);
+ printf("Level : %d\n",s1.level);
+ printf("Group : %s\n",s1.group);
+ 
+ printf("\n");
+ return 0;
+}
