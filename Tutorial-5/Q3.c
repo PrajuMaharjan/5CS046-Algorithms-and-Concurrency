4375f1b712e4d732dbf5d261e4a44f9531edb9d8
PrajuMaharjan <prajumaharjan903@gmail.com>
2026-04-02 08:05:32 +0545

    Tutorial-5 Q3 done

--- Tutorial-5/Q3.c
+++ Tutorial-5/Q3.c
@@ -1,1 +1,20 @@
 //3.Use of typedef keyword in c.
+
+#include <stdio.h>
+
+typedef struct {
+ char name[50];
+ int level;
+ char group[6];
+}Student;
+
+int main(){
+ Student s1={"Praju Maharjan",5,"L5CG9"};
+
+ printf("Name : %s\n",s1.name);
+ printf("Level : %d\n",s1.level);
+ printf("Group : %s\n",s1.group);
+
+ printf("\n");
+ return 0;
+}
